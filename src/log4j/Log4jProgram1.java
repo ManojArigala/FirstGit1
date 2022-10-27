c57f6244d432b1e0747549b103ccc9fdc0b55046
ManojArigala <arigalamanojgit2022@gmail.com>
2022-10-27 09:13:04 +0530

    pushcode

--- src/log4j/Log4jProgram1.java
+++ src/log4j/Log4jProgram1.java
@@ -17,7 +17,7 @@ public class Log4jProgram1 {
 	  PropertyConfigurator.configure("Log4j.properties");
 	  driver.get("https://www.google.com/");
 	  log.info("Enter google URL");
-	  log.info("Enter updated info");
+	  log.info("Enter date info");
   }
   
   @BeforeTest
