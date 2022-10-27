03d4ee4412c9578ac2e8eee9e049106b2403b809
ManojArigala <arigalamanojgit2022@gmail.com>
2022-10-27 09:04:29 +0530

    FirstCommit1

---
+++ src/log4j/Log4jProgram1.java
@@ -0,0 +1,29 @@
+package log4j;
+
+import org.testng.annotations.Test;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class Log4jProgram1 {
+	
+	
+	WebDriver driver;
+  @Test
+  public void f() {
+	  Logger log= Logger.getLogger("LogLearning");
+	  PropertyConfigurator.configure("Log4j.properties");
+	  driver.get("https://www.google.com/");
+	  log.info("Enter google URL");
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","D:\\Lib\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+  }
+
+}
